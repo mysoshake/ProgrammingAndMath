183e4765c3505e4e4e83cdf3d977773629e237d3
mysoshake <mysoshake@gmail.com>
2022-10-03 00:50:09 +1200

    add test

--- src/script/core/PAMCore.java
+++ src/script/core/PAMCore.java
@@ -1,5 +1,7 @@
 package core;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class PAMCore
 {
 	public static void main(String[] args)
@@ -10,6 +12,25 @@ public class PAMCore
 
 	public PAMCore()
 	{
+		System.out.println("Setup");
+		// TODO SETUP
+		System.out.println("Setup Finish");
+	}
 
+	public boolean Launch()
+	{
+		try
+		{
+			// TODO Call Launch
+			fail("Not Implemented");
+		}
+		catch (Exception e)
+		{
+			// TODO Error handle
+			System.err.println("Error");
+			return false;
+		}
+
+		return true;
 	}
 }
--- src/script/test/core/TestPAMCore.java
+++ src/script/test/core/TestPAMCore.java
@@ -1,12 +1,18 @@
 package test.core;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import core.PAMCore;
+
 class TestPAMCore
 {
 
 	@Test
 	void test()
 	{
-		fail("Not yet implemented");
+		assertTrue(new PAMCore().Launch());
 	}
 
 }
