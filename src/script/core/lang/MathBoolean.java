862e7fb63cbf255f62bae00a079cde83ad0b8160
mysoshake <mysoshake@gmail.com>
2022-10-04 21:44:48 +1200

    MathObject (未完成) MathBoolean (未完成)

---
+++ src/script/core/lang/MathBoolean.java
@@ -0,0 +1,13 @@
+package core.lang;
+
+public class MathBoolean extends MathObject
+{
+	public static final MathBoolean TRUE = new MathBoolean();
+
+	public static final MathBoolean FALSE = new MathBoolean();
+
+	public MathBoolean()
+	{
+
+	}
+}
---
+++ src/script/core/lang/MathObject.java
@@ -0,0 +1,16 @@
+package core.lang;
+
+public class MathObject
+{
+
+	public MathBoolean equals(MathObject mobj)
+	{
+		return MathBoolean.True;
+	}
+
+	public String toString()
+	{
+		return this.toString();
+	}
+
+}
