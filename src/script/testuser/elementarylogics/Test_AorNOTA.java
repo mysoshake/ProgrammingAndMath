e97faa3450cf14efc2a6dfca74da019bde799145
mysoshake <mysoshake@gmail.com>
2022-11-26 23:11:41 +0900

    Boolean Proposition change
    テストの追加
    Or　AndなどをDisjunction Conjunctionに対応

--- src/script/testuser/elementarylogics/Test_AorNOTA.java
+++ src/script/testuser/elementarylogics/Test_AorNOTA.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import lib.lang.constant.MathBoolean;
 import lib.lang.logic.MathProposition;
 
 class Test_AorNOTA
@@ -13,11 +12,54 @@ class Test_AorNOTA
 
 	@Test
 	@DisplayName("A∨￢A = T")
-	void test()
+	void test1()
 	{
-		MathProposition A = MathProposition.T;
+		MathProposition A;
 
-		assertEquals(MathProposition.T, A.Or);
+		A = MathProposition.T;
+		assertEquals(MathProposition.T, A.Or(A.Not()));
+
+		A = MathProposition.F;
+		assertEquals(MathProposition.T, A.Or(A.Not()));
+	}
+
+	@Test
+	@DisplayName("A⋀￢A = F")
+	void test2()
+	{
+		MathProposition A;
+
+		A = MathProposition.T;
+		assertEquals(MathProposition.F, A.And(A.Not()));
+
+		A = MathProposition.F;
+		assertEquals(MathProposition.F, A.And(A.Not()));
+	}
+
+	@Test
+	@DisplayName("A∨T = T")
+	void test3()
+	{
+		MathProposition A;
+
+		A = MathProposition.T;
+		assertEquals(MathProposition.T, A.Or(MathProposition.T));
+
+		A = MathProposition.F;
+		assertEquals(MathProposition.T, A.Or(MathProposition.T));
+	}
+
+	@Test
+	@DisplayName("A⋀T = F")
+	void test4()
+	{
+		MathProposition A;
+
+		A = MathProposition.T;
+		assertEquals(MathProposition.F, A.And(MathProposition.F));
+
+		A = MathProposition.F;
+		assertEquals(MathProposition.F, A.And(MathProposition.F));
 	}
 
 }
